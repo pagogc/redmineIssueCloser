4af2e1a5724bdb2f8d5e74a706d7b4b522f163d0
Michael Pagler <michael.pagler@softengine.at>
2024-01-31 10:23:06 +0100

    updated for other values and debug mode

--- redmineIssueCloser.py
+++ redmineIssueCloser.py
@@ -6,8 +6,12 @@ from datetime import datetime, timedelta, timezone
 import configparser
 import argparse
 
-# closes all issues in project "ticketsystem" (999_Ticketsystem) that are older then 7 days and on status 3 (gelöst)
-# needs url and token of redmine in a redmineIssueCloser.conf file or other file via --config argument
+# closes all issues in project "ticketsystem" (999_Ticketsystem) that are
+# - older then 14 days 
+# - on status 3 (gelöst) or on status 8 (warte auf Rückmeldung)
+
+# Needs url and token of redmine in a redmineIssueCloser.conf file or other file via --config argument
+# 
 
 parser = argparse.ArgumentParser(description='Description of your script.')
 parser.add_argument('--config', help='Other redmineIssueCloser.conf name (optional)')
@@ -23,6 +27,7 @@ config.read(configfile)
 # Access values from the 'Settings' section
 redmine_url = config.get('Settings', 'redmine_url')
 redmine_token = config.get('Settings', 'redmine_token')
+debug = config.getboolean('Settings', 'debug', fallback=False)
 
 if redmine_url is None:
     print(f"redmine_url not set")
@@ -32,11 +37,19 @@ if redmine_token is None:
     sys.exit()
 
 current_time = datetime.utcnow()
-seven_days_ago = current_time - timedelta(days=7)
+seven_days_ago = current_time - timedelta(days=14)
 timestamp_string = seven_days_ago.strftime('%Y-%m-%dT%H:%M:%SZ')
 filter_expression = "<=" + timestamp_string
 redmine = redminelib.Redmine(redmine_url, key=redmine_token)
 issues_to_close = redmine.issue.filter(project_id='ticketsystem',status_id=3, updated_on=filter_expression)
 for issue in issues_to_close:
     print(issue.id)
-    redmine.issue.update(issue.id,status_id=5)
+    if not debug:
+        redmine.issue.update(issue.id,status_id=5)
+
+print("------")
+issues_to_close = redmine.issue.filter(project_id='ticketsystem',status_id=8, updated_on=filter_expression)
+for issue in issues_to_close:
+    print(issue.id)
+    if not debug:
+        redmine.issue.update(issue.id,status_id=5)
